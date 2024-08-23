ecb7131908fccc6e3cb0b0af45e60ef983bb46da
Lev Alyukov <lev.alyukov@gmail.com>
2024-08-23 12:33:10 +0500

    update background.gd

--- assets/scripts/ui/main menu/background.gd
+++ assets/scripts/ui/main menu/background.gd
@@ -1,6 +1,18 @@
 extends ColorRect
 
+const dark:Color = Color("222831")
+const light:Color = Color("EEEEEE")
+const evening:int = 17
+const morning:int = 8
+
 func _ready():
-	print(
-		Time.get_time_string_from_system(false)
-	)
+	background(get_time())
+
+func background(time:String) -> void:
+	if !range(morning, evening).has(int(time.left(2))):
+		color = light
+	else:
+		color = dark
+
+func get_time() -> String:
+	return Time.get_time_string_from_system(false)
