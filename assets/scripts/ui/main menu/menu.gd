d8ad2c88d21c5339394cc68f05d6e332ff3b73c9
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-01 12:21:08 +0500

    update filesystem_cache

--- assets/scripts/ui/main menu/menu.gd
+++ assets/scripts/ui/main menu/menu.gd
@@ -8,7 +8,7 @@ var clicked:bool = true
 
 func _ready():
 	version.text = ProjectSettings.get_setting("application/config/version")
-	
+
 	blackout.visible = true
 	await get_tree().create_timer(1.25).timeout
 	blackout.blackout(false, 4)
