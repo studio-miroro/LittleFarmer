bbf106aed38f893afc8a8fb6536d1185db16945b
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-01 11:26:07 +0500

    update blur

--- assets/scripts/ui/blur.gd
+++ assets/scripts/ui/blur.gd
@@ -1,11 +1,12 @@
 extends Control
 
-@onready var main_scene = str(get_tree().root.get_child(1).name)
-@onready var hud = get_node("/root/" + main_scene + "/User Interface/Hud")
-@onready var tooltip = get_node("/root/" + main_scene + "/User Interface/System/Tooltip")
-@onready var player = get_node("/root/" + main_scene + "/Camera")
-
+@onready var main_scene:String = str(get_tree().root.get_child(1).name)
+@onready var hud:Control = get_node("/root/" + main_scene + "/User Interface/Hud")
+@onready var tooltip:Control = get_node("/root/" + main_scene + "/User Interface/System/Tooltip")
+@onready var camera:CharacterBody2D = get_node("/root/" + main_scene + "/Camera")
 @onready var background:ColorRect = $ColorRect
+
+var buildings:String = "/root/" + main_scene + "/Buildings/"
 
 const max_blur:float = 1.5
 const min_blur:float = 0.0
@@ -29,10 +30,9 @@ func blur(bluring:bool) -> void:
 	
 	hud._visible(bluring)
 	tooltip.tooltip()
-	player.switch = bluring
+	camera.switch = bluring
 
 	if bluring:
-		var parent = "/root/" + main_scene + "/Buildings/" 
-		if has_node(parent):
-			for nodes in get_node(parent).get_children():
+		if has_node(buildings):
+			for nodes in get_node(buildings).get_children():
 				nodes.change_sprite(false)
