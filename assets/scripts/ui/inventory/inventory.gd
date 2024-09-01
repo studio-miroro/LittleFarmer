51dd8768fae6100b3c6852974d4f8178854c8a6d
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-01 20:05:08 +0500

    update inventory

--- assets/scripts/ui/inventory/inventory.gd
+++ assets/scripts/ui/inventory/inventory.gd
@@ -7,6 +7,7 @@ extends Control
 @onready var build:Control = get_node("/root/" + main_scene + "/User Interface/Windows/Crafting")
 @onready var mailbox:Control = get_node("/root/" + main_scene + "/User Interface/Windows/Mailbox")
 @onready var storage:Node2D = get_node("/root/" + main_scene + "/Buildings/Storage")
+@onready var grid:Node2D = get_node("/root/" + main_scene + "/Buildings/Grid")
 @onready var node:PackedScene = load("res://assets/nodes/UI/Inventory/slot.tscn")
 @onready var anim:AnimationPlayer = $Animation
 
@@ -24,9 +25,13 @@ extends Control
 @onready var list:Label = $Panel/StorageItemList
 
 var menu:bool = false
-var inventory_items:Dictionary = {1:{"amount":100},2:{"amount":100},3:{"amount":100},4:{"amount":100},}
+var inventory_items:Dictionary = {13:{"amount":100},14:{"amount":100},15:{"amount":100},16:{"amount":2},}
+var item_index
+
+var button_index:int
 enum item_type {
-	seed,
+	NOTHING,
+	SEEDS,
 }
 
 func _ready():
@@ -38,7 +43,7 @@ func _process(_delta):
 		if Input.is_action_just_pressed("inventory"):
 			window()
 	else:
-		if (Input.is_action_just_pressed("pause") && menu) or Input.is_action_just_pressed("inventory"):
+		if (Input.is_action_just_pressed("pause") && menu) or (Input.is_action_just_pressed("inventory") && menu):
 			close()
 
 func open() -> void:
@@ -62,7 +67,9 @@ func items_load(data:Dictionary) -> void:
 func get_data(index) -> void:
 	if menu:
 		var item = Items.new()
+		item_index = index
 		scroll_info.scroll_vertical = 0
+
 		if item.content.has(int(index)):
 			if item.content[int(index)].has("icon"):
 				if typeof(item.content[int(index)]["icon"]) == TYPE_OBJECT:
@@ -171,7 +178,7 @@ func update_list() -> void:
 	else:
 		push_error("There is no parent of 'Buildings' in the '" + main_scene + "' scene")
 
-func get_all_items():
+func get_all_items() -> int:
 	if slots:
 		var item:int = 0
 		if inventory_items != {}:
@@ -180,6 +187,7 @@ func get_all_items():
 		return item
 	else:
 		push_error("Cannot load parent.")
+		return 0
 
 func add_item(id:int, amount:int) -> void:
 	if inventory_items.has(id):
@@ -187,10 +195,31 @@ func add_item(id:int, amount:int) -> void:
 	else:
 		inventory_items[id] = {"amount": amount}
 		
-func remove_item(id:int):
+func subject_item(item_id:int, item_amount:int):
+	for key in inventory_items:
+		if item_id == key:
+			inventory_items[item_id]["amount"] -= item_amount 
+
+func remove_item(id:int) -> void:
 	for key in inventory_items:
 		if id == key:
 			inventory_items.erase(key)
+
+func get_item_amount(item_id:int) -> int:
+	if inventory_items.has(item_id) and inventory_items[item_id].has("amount"):
+		if inventory_items[item_id]["amount"] > 0:
+			return inventory_items[item_id]["amount"]
+		else:
+			return 0
+	return 0
+
+func check_item_amount(item_id:int) -> bool:
+	if inventory_items.has(item_id) and inventory_items[item_id].has("amount"):
+		if inventory_items[item_id]["amount"] > 0:
+			return true
+		else:
+			return false
+	return false
 
 func check_amount(index) -> void:
 	if inventory_items.has(index):
@@ -200,7 +229,7 @@ func check_amount(index) -> void:
 			if inventory_items[index]["amount"] < 0:
 				inventory_items[index]["amount"] = 0
 		else:
-			push_warning("[ID: "+str(index)+"] The 'amount' element does not exist in the inventory dictionary (array).")
+			push_warning("[ID: " + str(index) + "] The 'amount' element does not exist in the inventory dictionary (array).")
 			inventory_items[index]["amount"] = 1
 
 func get_specifications(index, i) -> void:
@@ -224,9 +253,29 @@ func get_tip(tip:String) -> String:
 func check_item_type(i_type:String) -> void:
 	match i_type:
 		"Семена":
+			button_index = item_type.SEEDS
+			button.text = tr("Посадить семена")
 			button.visible = true
 		_:
+			button_index = item_type.NOTHING
 			button.visible = false
+
+func _on_button_pressed():
+	match button_index:
+		item_type.SEEDS:
+			close()
+			if Items.new().content.has(item_index):
+				if Items.new().content[item_index].has("crop"):
+					grid.inventory_plant = int(item_index)
+					grid.plantID = int(Items.new().content[item_index]["crop"])
+					grid.mode = grid.gridmode.SEEDS
+					grid.visible = true
+				else:
+					push_error("The 'crop' key does not exist")
+			else:
+				push_error("The numerical ID (" + item_index + ") of this crop is missing in the main file crops.gd")
+		_:
+			pass
 
 func window() -> void:
 	if menu:
@@ -236,9 +285,6 @@ func window() -> void:
 
 func check_window() -> void:
 	visible = menu
-
-func _on_button_pressed():
-	print("Test")
 
 func _on_close_pressed():
 	if menu:
