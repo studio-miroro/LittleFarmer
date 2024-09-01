bb884e33e41b222088dbc9bd25317877206e943f
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-01 12:18:05 +0500

    update blackout

--- assets/scripts/ui/blackout.gd
+++ assets/scripts/ui/blackout.gd
@@ -1,23 +1,34 @@
 extends Control
 
+@onready var background:ColorRect = $ColorRect
 @onready var anim:AnimationPlayer = $Animation
 
 func _ready():
 	z_index = 999
+	background.modulate = "ffffff"
 
 func blackout(state:bool, speed:int = 4) -> void:
 	match state:
 		true:
-			anim.play("Blackout")
+			anim.play("blackout")
 			anim.speed_scale = speed
 		false:
-			anim.play("Blackout_reset")
+			anim.play("blackout_reset")
 			anim.speed_scale = speed
+
+func change_color(colouring:Color, default_clear_color:bool = false):
+	if typeof(colouring) == TYPE_COLOR:
+		background.color = colouring
+		if default_clear_color:
+			RenderingServer.set_default_clear_color(colouring)
+	else:
+		push_error("The variable 'coloring' is not a type of 'Color'.")
 
 func change_scene(path:String) -> void:
 	if path != "":
 		await get_tree().create_timer(1.25).timeout
 		var result = get_tree().change_scene_to_file(path)
+		
 		if result == OK:
 			pass
 		else:
