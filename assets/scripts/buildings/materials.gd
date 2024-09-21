d63cb7fe77ce01258ab7f3c0bde80ad51f00f84a
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-21 17:03:57 +0500

    update materials.gd

--- assets/scripts/buildings/materials.gd
+++ assets/scripts/buildings/materials.gd
@@ -2,48 +2,15 @@ extends Node
 
 class_name BuildingMaterials
 var resources:Dictionary = {
-	"log": {
-		"caption": "Бревно",
-		"id": 1
-	},
-	"plank": {
-		"caption": "Доски",
-		"id": 2
-	},
-	"stone": {
-		"caption": "Камень",
-		"id": 3
-	},
-	"cobblestone": {
-		"caption": "Булыжник",
-		"id": 4
-	},
-	"coal": {
-		"caption": "Уголь",
-		"id": 5
-	},
-	"copper_ore": {
-		"caption": "Медная руда",
-		"id": 7
-	},
-	"copper_ingot": {
-		"caption": "Медный слиток",
-		"id": 8
-	},
-	"iron_ore": {
-		"caption": "Железная руда",
-		"id": 9
-	},
-	"iron_ingot": {
-		"caption": "Железный слиток",
-		"id": 10
-	},
-	"aluminum_ore": {
-		"caption": "Бокситовая руда",
-		"id": 11
-	},
-	"aluminum": {
-		"caption": "Алюминий",
-		"id": 12
-	},
+	"log": 1,
+	"plank": 2,
+	"stone": 3,
+	"cobblestone": 4,
+	"coal": 5,
+	"copper_ore": 7,
+	"copper_ingot": 8,
+	"iron_ore": 9,
+	"iron_ingot": 10,
+	"aluminum_ore": 11,
+	"aluminum": 12,
 }
