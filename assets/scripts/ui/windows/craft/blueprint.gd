acf059769b41c7d5351f64f4427ab7b83cb3eef2
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-22 13:41:03 +0500

    fix logging system in blueprint.gd

--- assets/scripts/ui/windows/craft/blueprint.gd
+++ assets/scripts/ui/windows/craft/blueprint.gd
@@ -1,7 +1,8 @@
 extends Control
 
 @onready var main_scene = str(get_tree().root.get_child(1).name)
-@onready var build = get_node("/root/" + main_scene + "/User Interface/Windows/Crafting")
+@onready var manager:Node2D = get_node("/root/" + main_scene)
+@onready var build:Control = get_node("/root/" + main_scene + "/User Interface/Windows/Crafting")
 @onready var icon:TextureRect = $Button/HBoxContainer/MarginContainer/Icon
 @onready var caption:Label = $Button/HBoxContainer/Caption
 @onready var index:int
@@ -15,20 +16,20 @@ func set_data(id) -> void:
 			if typeof(blueprints.content[id]["caption"]) == TYPE_STRING and caption.text is String:
 				caption.text = str(blueprints.content[index]["caption"])
 			else:
-				push_error("The 'caption' key has a non-string type. Variant.type: " + str(typeof(blueprints.content[id]["caption"])))
+				print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: The 'caption' key has a non-string type. Variant.type: " + str(typeof(blueprints.content[id]["caption"])))
 		else:
-			push_error("The object does not have the 'caption' key.")
+			print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: The object does not have the 'caption' key.")
 
 		if blueprints.content[id].has("icon"):
 			if typeof(blueprints.content[id]["icon"]) == TYPE_OBJECT and icon.texture is CompressedTexture2D:
 				icon.texture = blueprints.content[index]["icon"]
 			else:
-				push_error("The key stores a non-Compressed 2D Texture. Variant.type: " + str(typeof(blueprints.content[id]["icon"])))
+				print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: The key stores a non-Compressed 2D Texture. Variant.type: " + str(typeof(blueprints.content[id]["icon"])))
 		else:
-			push_error("The object does not have the 'icon' key.")
+			print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: The object does not have the 'icon' key.")
 		visible = true
 	else:
-		push_error("Invalid object index: " + str(id))
+		print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: Invalid object index: " + str(id))
 		queue_free()
 
 func check_node(id) -> bool:
