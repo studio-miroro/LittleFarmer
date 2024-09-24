c4b79303f365219ea8d1e3d1c2eb4fda59455f5c
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-24 10:11:46 +0500

    update building system

--- assets/nodes/ui/windows/craft/craft_button.gd
+++ assets/nodes/ui/windows/craft/craft_button.gd
@@ -1,11 +1,14 @@
 extends Button
 
 @onready var main_scene = str(get_tree().root.get_child(1).name)
+@onready var manager:Node2D = get_node("/root/" + main_scene)
 @onready var pause:Control = get_node("/root/" + main_scene + "/User Interface/Windows/Pause")
 @onready var notice:Control = get_node("/root/" + main_scene + "/User Interface/System/Notifications")
+@onready var hud:Control = get_node("/root/" + main_scene + "/User Interface/Hud")
 @onready var inventory:Control = get_node("/root/" + main_scene + "/User Interface/Windows/Inventory")
 @onready var craft:Control = get_node("/root/" + main_scene + "/User Interface/Windows/Crafting")
 @onready var blur:Control = get_node("/root/" + main_scene + "/User Interface/Blur")
+@onready var grid:Node2D = get_node("/root/" + main_scene + "/Buildings/Grid")
 
 var items:Object = Items.new()
 var blueprints:Object = Blueprints.new()
@@ -19,19 +22,64 @@ func _on_pressed():
 		build(id)
 
 func build(index:int) -> void:
-	check_building_type(index)
-			
-func check_building_type(index:int):
+	start_building(index)
+
+func start_building(index) -> void:
 	var target_array = blueprints.content[index]["type"].keys() 
 	match target_array[0]:
 		"terrain":
-			# Code ...
-			craft.close()
+			if blueprints.content[index]["type"]["terrain"].has("terrain_set"):
+				if blueprints.content[index]["type"]["terrain"].has("terrain_layer"):
+					grid.terrain_set = blueprints.content[index]["type"]["terrain"]["terrain_set"]
+					grid.terrain_layer = blueprints.content[index]["type"]["terrain"]["terrain_layer"]
+					grid.mode = grid.modes.TERRAIN_SET
+					grid.visible = true
+					craft.close()
+					hud.state(true)
+				else:
+					print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: The key element is missing - 'terrain_layer'")
+					reset_grid_data(target_array[0])
+			else:
+				print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: The key element is missing - 'terrain_set'")
+				reset_grid_data(target_array[0])
+
 		"node":
-			# Code ...
-			craft.close()
-		"upgrade":
-			# Code ...
-			craft.close()
+			if blueprints.content[index]["type"]["node"].has("source"):
+				if blueprints.content[index]["type"]["node"].has("layer"):
+					if blueprints.content[index]["type"]["node"].has("shadow"):
+						grid.building_id = index
+						grid.building_node = blueprints.content[index]["type"]["node"]["source"]
+						grid.building_node_layer = blueprints.content[index]["type"]["node"]["layer"]
+						grid.building_shadow = blueprints.content[index]["type"]["node"]["shadow"]
+						grid.mode = grid.modes.BUILD
+						grid.visible = true
+						craft.close()
+						hud.state(true)
+					else:
+						print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: The key element is missing - 'shadow'")
+						reset_grid_data(target_array[0])
+				else:
+					print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: The key element is missing - 'layer'")
+					reset_grid_data(target_array[0])
+			else:
+				print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: The key element is missing - 'source'")
+				reset_grid_data(target_array[0])		
+
+		_:
+			print_debug("\n"+str(manager.get_system_datetime()) + " ERROR: Invalid type - "+str(target_array[0]))
+			reset_grid_data(target_array[0])
+
+
+func reset_grid_data(index) -> void:
+	match index:
+		"terrain":
+			grid.terrain_set = 0
+			grid.terrain_layer = 0
+			grid.mode = grid.modes.NOTHING
+			grid.visible = false
+		"node":
+			grid.building_node = null
+			grid.mode = grid.modes.NOTHING
+			grid.visible = false
 		_:
 			pass
--- assets/scripts/buildings/buildings.gd
+++ assets/scripts/buildings/buildings.gd
@@ -1,19 +1,33 @@
 extends Node2D
 
 @onready var main_scene = str(get_tree().root.get_child(1).name)
+@onready var manager = get_node("/root/" + main_scene)
 @onready var buildings:Node2D = get_node("/root/" + main_scene + "/Buildings")
+@onready var tilemap:Node2D = get_node("/root/" + main_scene + "/Tilemap")
+@onready var grid:Node2D = get_node("/root/" + main_scene + "/Buildings/Grid")
+@onready var collision:Node2D = get_node("/root/" + main_scene + "/Buildings/Grid/GridCollision")
 
 func get_buildings() -> Dictionary:
 	var data_dict = {}
-	for build in buildings.get_children():
-		if build.has_method("get_data"):
-			data_dict[build.name] = build.get_data()
+	for building in buildings.get_children():
+		if building.has_method("get_data"):
+			data_dict[building.name] = building.get_data()
 	return data_dict
 
 func build_content(build_name:String, level:int) -> void:
-	for build in buildings.get_children():
-		if build.name == build_name:
-			if build.has_method("load_data"):
-				build.load_data(level)
+	for building in buildings.get_children():
+		if building.name == build_name:
+			if building.has_method("load_data"):
+				building.load_data(level)
 			else:
-				push_error("The '" + str(build.name) + "' node does not have the 'load_data' method.")
+				push_error("The '" + str(building.name) + "' node does not have the 'load_data' method.")
+
+func build(tile_mouse_pos:Vector2i, building_node:PackedScene, building_layer:int, node_shadow:CompressedTexture2D) -> void:
+	var shadow_group:CanvasGroup = get_node("/root/" + main_scene + "/Shadow")
+	var node:Node2D = building_node.instantiate()
+
+	tilemap.set_cell(building_layer, tile_mouse_pos, 0, Vector2i(0,3))
+	node.set_position(tilemap.map_to_local(tile_mouse_pos))
+	node.z_index = 6
+	add_child(node)
+	shadow_group.create_shadow(tile_mouse_pos, node_shadow)
