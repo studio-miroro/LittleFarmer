9af2402f469a15f6654acbf0cdd21e9181def1f7
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-27 15:26:52 +0500

    bugs fix in teleport.gd

--- assets/scripts/world/teleports.gd
+++ assets/scripts/world/teleports.gd
@@ -1,34 +1,38 @@
 extends Node2D
 
 @onready var main_scene = str(get_tree().root.get_child(1).name)
-
 @onready var manager = get_node("/root/" + main_scene)
 @onready var blackout:Control = get_node("/root/" + main_scene + "/User Interface/Blackout")
+@onready var grid:Node2D = get_node("/root/" + main_scene + "/Buildings/Grid")
 
 var teleporting:bool
 
 func _input(event) -> void:
 	if event is InputEventMouseButton\
-	and event.button_index == MOUSE_BUTTON_LEFT\
-	and event.is_pressed()\
-	and teleporting:
+	&& event.button_index == MOUSE_BUTTON_LEFT\
+	&& event.is_pressed()\
+	&& teleporting\
+	&& grid.mode == grid.modes.NOTHING:
 		teleport()
 
 func teleport() -> void:
 	blackout.blackout(true)
-	
-	if main_scene == "Farm":
-		var path:String = "res://levels/village.tscn"
-		manager.gamesave()
-		GameLoader.loading(false)
-		blackout.change_scene(path)
+	match main_scene:
+		"Farm":
+			var path:String = "res://levels/village.tscn"
+			manager.gamesave()
+			GameLoader.loading(false)
+			blackout.change_scene(path)
 
-	if main_scene == "Village":
-		var path:String = "res://levels/farm.tscn"
-		GameLoader.loading(true)
-		manager.file_save(manager.paths.world, "nature")
-		manager.file_save(manager.paths.player, "player")
-		blackout.change_scene(path)
+		"Village":
+			var path:String = "res://levels/farm.tscn"
+			GameLoader.loading(true)
+			manager.file_save(manager.paths.world, "nature")
+			manager.file_save(manager.paths.player, "player")
+			blackout.change_scene(path)
+
+		_:
+			manager.debug("Pizda")
 
 func _on_area_2d_mouse_entered():
 	teleporting = true
