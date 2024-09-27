25a612ba4128ee55b21b8ac1e66aab9e47e42e05
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-27 17:53:17 +0500

    add global tilemap.gd file

---
+++ assets/scripts/tilemap.gd
@@ -0,0 +1,17 @@
+extends TileMap
+
+@onready var main_scene = str(get_tree().root.get_child(1).name)
+@onready var data = get_node("/root/" + main_scene)
+@onready var blur:Control = get_node("/root/" + main_scene + "/User Interface/Blur")
+@onready var grid:Node2D = get_node("/root/" + main_scene + "/Buildings/Grid")
+
+func _process(_delta):
+	if !blur.state:
+		if has_node("/root/" + main_scene + "/Buildings"):
+			if has_node("/root/" + main_scene + "/Buildings/Grid"):
+				if grid.mode != grid.modes.NOTHING:
+					grid_movement()
+
+func grid_movement() -> void:
+	var movement:Vector2 = local_to_map(get_global_mouse_position())
+	grid.set_position(map_to_local(movement))
