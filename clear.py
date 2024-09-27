f6262d64a7ca470790cd2c9a2570789e9e132055
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-27 15:18:44 +0500

    update clear.py

--- clear.py
+++ clear.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 import_dir = "./.import"
-extensions = [".cfg", ".tscn-", ".tmp", ".save", ".dmp"]
+extensions = [".cfg", ".tscn-", ".tmp", ".save", ".dmp", ".ctex"]
 protected_files = [
     "global_script_class_cache.cfg", 
     "project_metadata.cfg", 
