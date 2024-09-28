84ce16495824654dc18bffc78e82497caa6979e5
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-28 19:12:35 +0500

    update buttons in main menu

--- assets/scripts/ui/main_menu/buttons/options_button.gd
+++ assets/scripts/ui/main_menu/buttons/options_button.gd
@@ -2,21 +2,21 @@ extends Control
 
 @onready var main_scene = str(get_tree().root.get_child(1).name)
 @onready var menu:Node2D = get_node("/root/" + main_scene)
+@onready var blur:Control = get_node("/root/" + main_scene +"/Canvas/Options/Blur")
 @onready var blackout:Control = get_node("/root/" + main_scene +"/Canvas/MainMenu/Blackout")
+@onready var options:Control = get_node("/root/" + main_scene +"/Canvas/Options/Options")
 @onready var sprite:TextureRect = $VBoxContainer/Sprite/ButtonSprite
 @onready var caption:Label = $VBoxContainer/Label/Scroll/Label
 @onready var anim:AnimationPlayer = $AnimationPlayer
 @onready var button:Button = $Button
-var path:String = "res://levels/farm.tscn"
 var button_state:bool
 
 func _on_button_pressed():
 	if !menu.clicked:
 		menu.clicked = true
-		blackout.blackout(true)
-		await get_tree().create_timer(1.25).timeout
-		GameLoader.loading(false)
-		get_tree().change_scene_to_file(path)
+		menu.change_button_state(true)
+		blur.blur(true)
+		options.open()
 
 func buttons_state(state:bool) -> void:
 	match state:
@@ -35,3 +35,4 @@ func _show() -> void:
 
 func _check_window() -> void:
 	visible = button_state
+
--- assets/scripts/ui/main_menu/buttons/quit.gd
+++ assets/scripts/ui/main_menu/buttons/quit.gd
@@ -1,9 +1,13 @@
 extends Control
 
-@onready var menu:Node2D = get_node("/root/" + str(get_tree().root.get_child(1).name))
-@onready var blackout:Control = get_node("/root/Menu/CanvasLayer/Blackout")
+@onready var main_scene = str(get_tree().root.get_child(1).name)
+@onready var menu:Node2D = get_node("/root/" + main_scene)
+@onready var blackout:Control = get_node("/root/" + main_scene +"/Canvas/MainMenu/Blackout")
 @onready var sprite:TextureRect = $VBoxContainer/Sprite/ButtonSprite
 @onready var caption:Label = $VBoxContainer/Label/Scroll/Label
+@onready var anim:AnimationPlayer = $AnimationPlayer
+@onready var button:Button = $Button
+var button_state:bool
 
 func _on_button_pressed():
 	if !menu.clicked:
@@ -11,3 +15,21 @@ func _on_button_pressed():
 		blackout.blackout(true)
 		await get_tree().create_timer(1.25).timeout
 		get_tree().quit()
+
+func buttons_state(state:bool) -> void:
+	match state:
+		true:
+			_hide()
+		false:
+			_show()
+
+func _hide() -> void:
+	anim.play("hide")
+	button_state = false
+
+func _show() -> void:
+	anim.play("show")
+	button_state = !false
+
+func _check_window() -> void:
+	visible = button_state
