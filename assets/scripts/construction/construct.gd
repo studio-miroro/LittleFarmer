d7b65304bc09c4c264d0fa0c32c3054524002080
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-30 19:50:26 +0500

    building.gd -> construct.gd

---
+++ assets/scripts/construction/construct.gd
@@ -0,0 +1,40 @@
+extends Node
+
+@onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var data:Node2D = get_node("/root/"+main)
+@onready var inventory:Control = get_node("/root/"+main+"/User Interface/Windows/Inventory")
+@onready var buildings:Node = get_node("/root/"+main+"/ConstructionManager")
+@onready var shadows:Node = get_node("/root/"+main+"/ShadowManager")
+@onready var tilemap:Node2D = get_node("/root/"+main+"/Tilemap")
+@onready var grid:Node2D = get_node("/root/"+main+"/ConstructionManager/Grid")
+@onready var collision:Node2D = get_node("/root/"+main+"/ConstructionManager/Grid/GridCollision")
+const max_distance:int = 250
+
+func get_buildings() -> Dictionary:
+	var data_dict = {}
+	for building in buildings.get_children():
+		if building.has_method("get_data"):
+			data_dict[building.name] = building.get_data()
+	return data_dict
+
+func build_content(build_name:String, level:int) -> void:
+	for building in buildings.get_children():
+		if building.name == build_name:
+			if building.has_method("load_data"):
+				building.load_data(level)
+			else:
+				data.debug("The '" + str(building.name) + "' node does not have the 'load_data' method, skipped.", "error")
+
+func construct(node_id:int, node_scene:PackedScene, node_shadow:CompressedTexture2D, node_position:Vector2i) -> void:
+	var building:Node2D = node_scene.instantiate()
+	var blueprints = Blueprints.new()
+	var building_name = "build"
+	if !collision.building_collision_check(collision.building_layer):
+		tilemap.set_cell(collision.building_layer, node_position, 0, Vector2i(0,3))
+		building.set_position(tilemap.map_to_local(node_position))
+		building.z_index = collision.building_layer
+		add_child(building)
+		if blueprints.content[node_id]["type"]["node"].has("name"):
+			building_name = blueprints.content[node_id]["type"]["node"]["name"] + "_1"
+			building.name = building_name
+		shadows.create_shadow(building_name, node_shadow, node_position)
