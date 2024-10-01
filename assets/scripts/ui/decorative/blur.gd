221aeceab3a9ea52eb9c419dc83b67a96d979f69
Lev Alyukov <lev.alyukov@gmail.com>
2024-10-01 20:47:54 +0500

    fix blur.gd

--- assets/scripts/ui/decorative/blur.gd
+++ assets/scripts/ui/decorative/blur.gd
@@ -1,10 +1,11 @@
 extends Control
 
-@onready var main_scene:String = str(get_tree().root.get_child(1).name)
-@onready var hud:Control = get_node("/root/" + main_scene + "/UI/HUD/GameHud")
-@onready var tooltip:Control = get_node("/root/" + main_scene + "/UI/Feedback/Tooltip")
-@onready var camera:CharacterBody2D = get_node("/root/" + main_scene + "/Player")
-@onready var zoom:Camera2D = get_node("/root/" + main_scene + "/Player/Camera2D")
+@onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var data = get_node("/root/"+main)
+@onready var hud:Control = get_node("/root/"+main+"/UI/HUD/GameHud")
+@onready var tooltip:Control = get_node("/root/"+main+"/UI/Feedback/Tooltip")
+@onready var camera:CharacterBody2D = get_node("/root/"+main+"/Player")
+@onready var zoom:Camera2D = get_node("/root/"+main+"/Player/Camera2D")
 @onready var background:ColorRect = $ColorRect
 
 const max_blur:float = 1.5
@@ -26,16 +27,18 @@ func _process(_delta):
 func blur(bluring:bool) -> void:
 	self.state = bluring
 
-	if has_node("/root/" + main_scene + "/UI/HUD/GameHud"):
+	if has_node("/root/"+main+"/UI/HUD/GameHud"):
 		hud.state(bluring)
-	if has_node("/root/" + main_scene + "/UI/Feedback/Tooltip"):
+	if has_node("/root/"+main+"/UI/Feedback/Tooltip"):
 		tooltip.tooltip()
-	if has_node("/root/" + main_scene + "/Player"):
+	if has_node("/root/"+main+"/Player"):
 		camera.switch = bluring
-	if has_node("/root/" + main_scene + "/Player/Camera2D"):
+	if has_node("/root/"+main+"/Player/Camera2D"):
 		zoom.change_zoom = !bluring
 	if bluring:
-		if has_node("/root/" + main_scene + "/Buildings/"):
-			for nodes in get_node("/root/" + main_scene + "/Buildings/").get_children():
+		if has_node("/root/"+main+"/ConstructionManager/"):
+			for nodes in get_node("/root/"+main+"/ConstructionManager/").get_children():
 				if nodes.has_method("_change_sprite"):
 					nodes._change_sprite(false)
+		else:
+			data.debug("Invalid parent: 'ConstructionManager'", "error")
