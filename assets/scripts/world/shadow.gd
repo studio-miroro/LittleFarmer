c3e2a30ba721baa97a62bda53b02651f39b4eaed
Lev Alyukov <lev.alyukov@gmail.com>
2024-10-01 21:14:33 +0500

    fix shadow.gd

--- assets/scripts/world/shadow.gd
+++ assets/scripts/world/shadow.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @onready var main:String = str(get_tree().root.get_child(1).name)
-@onready var data:Node2D = get_node("/root/"+main)
+@onready var data:Node = get_node("/root/"+main)
 @onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
 @onready var collision:Node2D = get_node("/root/"+main+"/ConstructionManager/Grid/GridCollision")
 @onready var canvas:CanvasGroup = $CanvasGroup
@@ -10,12 +10,12 @@ const alpha:float = .25
 const color:Color = Color(0,0,0,alpha)
 
 func create_shadow(shadow_name:String, shadow_texture:CompressedTexture2D, shadow_position:Vector2i) -> void:
-    if shadow_name == "":
-        shadow_name = "shadow"
-    var shadow:Sprite2D = Sprite2D.new()
-    shadow.name = shadow_name
-    shadow.texture = shadow_texture
-    shadow.position = tilemap.map_to_local(shadow_position)
-    shadow.z_index = collision.shadow_layer
-    canvas.add_child(shadow)
-    canvas.modulate = color
+	if shadow_name == "":
+		shadow_name = "shadow"
+	var shadow:Sprite2D = Sprite2D.new()
+	shadow.name = shadow_name
+	shadow.texture = shadow_texture
+	shadow.position = tilemap.map_to_local(shadow_position)
+	shadow.z_index = collision.shadow_layer
+	canvas.add_child(shadow)
+	canvas.modulate = color
