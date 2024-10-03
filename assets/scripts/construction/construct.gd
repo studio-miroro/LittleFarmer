dcc25efe4dff0dbaeb5e17599447d26330c72c87
Lev Alyukov <lev.alyukov@gmail.com>
2024-10-03 09:34:42 +0500

    update construct.gd

--- assets/scripts/construction/construct.gd
+++ assets/scripts/construction/construct.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var main = str(get_tree().root.get_child(1).name)
 @onready var data = get_node("/root/"+main)
-@onready var inventory:Control = get_node("/root/"+main+"/User Interface/Windows/Inventory")
+@onready var inventory:Control = get_node("/root/"+main+"/UI/Interactive/Inventory")
 @onready var buildings:Node = get_node("/root/"+main+"/ConstructionManager")
 @onready var shadows:Node = get_node("/root/"+main+"/ShadowManager")
 @onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
@@ -29,12 +29,12 @@ func construct(node_id:int, node_scene:PackedScene, node_shadow:CompressedTextur
 	var building:Node2D = node_scene.instantiate()
 	var blueprints = Blueprints.new()
 	var building_name = "build"
-	if !collision.building_collision_check(collision.building_layer):
-		tilemap.set_cell(collision.building_layer, node_position, 0, Vector2i(0,3))
-		building.set_position(tilemap.map_to_local(node_position))
-		building.z_index = collision.building_layer
-		add_child(building)
-		if blueprints.content[node_id]["type"]["node"].has("name"):
-			building_name = blueprints.content[node_id]["type"]["node"]["name"] + "_1"
-			building.name = building_name
-		shadows.create_shadow(building_name, node_shadow, node_position)
+
+	add_child(building)
+	tilemap.set_cell(collision.building_layer-1, node_position, 0, Vector2i(0,3))
+	building.set_position(tilemap.map_to_local(node_position))
+	building.z_index = collision.building_layer
+	if blueprints.content[node_id]["type"]["node"].has("name"):
+		building_name = blueprints.content[node_id]["type"]["node"]["name"] + "_1"
+		building.name = building_name
+	shadows.create_shadow(building_name, node_shadow, node_position)
