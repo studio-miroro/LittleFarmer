3ea22eb6f7bb872197b97de7912a4c5650200739
Lev Alyukov <lev.alyukov@gmail.com>
2024-10-05 19:13:52 +0500

    add new tool - harvesting

---
+++ assets/scripts/ui/hud/instruments/button_harvesting.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
+@onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
+@onready var grid:Node2D = get_node("/root/"+main+"/ConstructionManager/Grid")
+
+func _on_button_pressed() -> void:
+	if !pause.lock:
+		if has_node("/root/"+main+"/ConstructionManager")\
+		&& has_node("/root/"+main+"/ConstructionManager/Grid"):
+			if !blur.state:
+				grid.mode = grid.modes.HARVESTING
+				grid.visible = true
