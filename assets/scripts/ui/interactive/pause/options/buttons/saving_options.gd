8e07ec2aa1f7925326387658d4885f2f96925459
Lev Alyukov <lev.alyukov@gmail.com>
2024-10-11 17:45:35 +0500

    fix: saving_options.gd

--- assets/scripts/ui/interactive/pause/options/buttons/saving_options.gd
+++ assets/scripts/ui/interactive/pause/options/buttons/saving_options.gd
@@ -1,18 +1,18 @@
 extends Control
 
-@onready var main_scene = str(get_tree().root.get_child(1).name)
-@onready var pause:Control = get_node("/root/" + main_scene + "/UI/Windows/Pause")
+@onready var main = str(get_tree().root.get_child(1).name)
+@onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 
 func _on_button_pressed():
-	if main_scene != "MainMenu":
-		var options:Control = get_node("/root/" + main_scene + "/UI/Windows/Options")
+	if main != "MainMenu":
+		var options:Control = get_node("/root/"+main+"/UI/Interactive/Options")
 		if options.menu:
 			options.close()
 			pause.visible = true
 	else:
-		var options:Control = get_node("/root/" + main_scene +"/Canvas/Options/Options")
-		var blur:Control = get_node("/root/" + main_scene +"/Canvas/Options/Blur")
-		var menu:Node2D = get_node("/root/" + main_scene)
+		var options:Control = get_node("/root/"+main+"/Canvas/Options/Options")
+		var blur:Control = get_node("/root/"+main+"/Canvas/Options/Blur")
+		var menu:Node2D = get_node("/root/"+main)
 
 		options.close()
 		blur.blur(false)
