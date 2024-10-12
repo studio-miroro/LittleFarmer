0c86f8da787478d411bd22b32ccf26baf21dd6cf
Lev Alyukov <lev.alyukov@gmail.com>
2024-10-12 11:05:43 +0500

    fix: farming.gd

--- assets/scripts/farming/farming.gd
+++ assets/scripts/farming/farming.gd
@@ -7,22 +7,12 @@ extends Node2D
 
 func create_plant(id:int, pos:Vector2i) -> void:
 	var plant = node.instantiate()
-	var mouse_position = tilemap.local_to_map(get_global_mouse_position())
 	var atlas_coords = Vector2i(0,3)
 	var source_id = 0
 	
-	if collision.check_cell(pos, collision.farmland_layer)\
-	&& !collision.check_cell(pos, collision.watering_layer):
+	if collision.check_cell(pos, collision.farmland_layer):
 		tilemap.set_cell(collision.crops_layer,pos,source_id,atlas_coords)
-		plant.set_position(tilemap.map_to_local(mouse_position))
-		add_child(plant)
-		plant.z_index = 6
-		plant.name = "plant_1"
-		plant.plant(id)
-		plant.check(id,pos)
-	else:
-		tilemap.set_cell(collision.crops_layer,pos,source_id,atlas_coords)
-		plant.set_position(tilemap.map_to_local(mouse_position))
+		plant.set_position(tilemap.map_to_local(pos))
 		add_child(plant)
 		plant.z_index = 6
 		plant.name = "plant_1"
