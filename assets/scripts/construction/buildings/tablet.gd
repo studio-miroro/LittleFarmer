26872135073a6a4c563ddd6f39e5fdab7b791607
Lev Alyukov <lev.alyukov@gmail.com>
2024-10-16 10:03:08 +0500

    update: tablet.gd

--- assets/scripts/construction/buildings/tablet.gd
+++ assets/scripts/construction/buildings/tablet.gd
@@ -2,22 +2,31 @@ extends Node2D
 
 @onready var main:String = str(get_tree().root.get_child(1).name)
 @onready var data:Node2D = get_node("/root/"+main)
+@onready var canvas:Node = get_node("/root/"+main+"/ShadowManager")
+@onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
 @onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
-@onready var pause:Control = get_node("/root/"+main+"/UI/Inveractive/Pause")
+@onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var tip:Control = get_node("/root/"+main+"/UI/Feedback/Tooltip")
+@onready var building:Node = get_node("/root/"+main+"/ConstructionManager")
 @onready var grid:Node2D = get_node("/root/"+main+"/ConstructionManager/Grid")
 @onready var player:CharacterBody2D = get_node("/root/"+main+"/Player")
 @onready var sprite:Sprite2D = $Sprite2D
 
-var max_distance:int = 250
+#114418401
+#137627939
 var object:Dictionary = {
 	"caption" = tr("tablet.caption"),
 	"description" = tr("tablet.description"),
 	"default" = load("res://assets/resources/buildings/tablet/tablet_0.png"),
 	"hover" = load("res://assets/resources/buildings/tablet/tablet_1.png"),
+	"shadow" = load("res://assets/resources/buildings/tablet/shadow.png")
 }
 
 func _ready():
+	update()
+	_shadow_create()
+
+func update() -> void:
 	if object.has("default"):
 		if object["default"] is CompressedTexture2D:
 			sprite.texture = object["default"]
@@ -26,10 +35,22 @@ func _ready():
 	else:
 		data.debug("The specified key is missing.", "error")
 
+func _shadow_create() -> void:
+	if visible:
+		if object.has("shadow"):
+			if object["shadow"] is CompressedTexture2D:
+				var vector2i_position = tilemap.local_to_map(position)
+				var target_position = Vector2i(vector2i_position.x, vector2i_position.y)
+				canvas.create_shadow("tablet_shadow", object["shadow"], target_position)
+			else:
+				data.debug("It is not possible to create a game shadow of an object because the sprite is not of the 'CompressedTexture2D' type.", "error")
+		else:
+			data.debug("The 'shadow' element is missing", "error")
+
 func _change_sprite(type:bool) -> void:
 	if type:
 		var distance = round(global_position.distance_to(player.global_position))
-		if grid.mode == grid.modes.NOTHING and distance < max_distance:
+		if grid.mode == grid.modes.NOTHING and distance < building.max_distance:
 			_check_sprite("hover")
 			if object.has("caption")\
 			and object.has("description"):
