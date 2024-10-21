6b1cb002a3fb5498a24215c77aabefe8bd23630b
Lev Alyukov <lev.alyukov@gmail.com>
2024-10-21 21:42:58 +0500

    update: cloud.gd

--- assets/scripts/world/nature/cloud.gd
+++ assets/scripts/world/nature/cloud.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 @onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var player:CharacterBody2D = get_node("/root/"+main+"/Player")
 @onready var clock:Control = get_node("/root/"+main+"/UI/HUD/GameHud/Main/Bars/Clock")
 @onready var shadow:Node = get_node("/root/"+main+"/ShadowManager")
@@ -8,18 +9,19 @@ extends Sprite2D
 @onready var life:Timer = $LifeCycle
 
 const speed:float = 24.256
-const max_distance:int = 500
+const max_distance:int = 400
 
 func _ready():
 	life.wait_time = randi_range(2*clock.speed,10*clock.speed)
 	life.start()
 
 func _process(delta) -> void:
-	position.x += speed * delta
-	position.y += speed * delta
-	var distance = round(global_position.distance_to(player.global_position))
-	if distance > max_distance:
-		change_animation(false)
+	if !pause.paused:
+		position.x += speed * delta
+		position.y += speed * delta
+		var distance = round(global_position.distance_to(player.global_position))
+		if distance > max_distance:
+			change_animation(false)
 
 func change_animation(state:bool) -> void:
 	if state:
