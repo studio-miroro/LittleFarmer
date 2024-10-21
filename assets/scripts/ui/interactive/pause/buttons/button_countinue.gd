37f69441b801a8945e9e75b79ab974ac83136a2a
Lev Alyukov <lev.alyukov@gmail.com>
2024-10-21 21:42:22 +0500

    update: button_countinue.gd

--- assets/scripts/ui/interactive/pause/buttons/button_countinue.gd
+++ assets/scripts/ui/interactive/pause/buttons/button_countinue.gd
@@ -1,9 +1,9 @@
 extends Button
 
-@onready var main_scene = str(get_tree().root.get_child(1).name)
-@onready var blur:Control = get_node("/root/" + main_scene + "/UI/Decorative/Blur")
-@onready var pause:Control = get_node("/root/" + main_scene + "/UI/Interactive/Pause")
-@onready var player:CharacterBody2D = get_node("/root/" + main_scene + "/Camera")
+@onready var main = str(get_tree().root.get_child(1).name)
+@onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
+@onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
+@onready var player:CharacterBody2D = get_node("/root/"+main+"/Player")
 
 func _on_pressed() -> void:
 	if blur.state:
